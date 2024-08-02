46bf50a0d875b40bd3fb6ad7fae4493b0c62c5ad
Charan Nunnabhatla <Charan.reings125@gmail.com>
2024-08-02 20:32:53 +0530

    intial setup

--- src/App.js
+++ src/App.js
@@ -7,7 +7,9 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          <code>console.log(4+)</code>
+          console.log(4+)
+          {4+4}
         </p>
         <a
           className="App-link"
