91694cdb2c751d0250ee907028ba08213869d0c8
charan <charan.reings125@gmail.com>
2024-12-15 19:38:21 +0530

    adjusted

--- src/components/custom/grid-layout.tsx
+++ src/components/custom/grid-layout.tsx
@@ -6,7 +6,7 @@ type GridLayoutType = {
 export default function GridLayout({ total, color }: GridLayoutType) {
   return (
     <div
-      className={`flex gap-2 p-2 m-3 justify-center items-center flex-wrap h-fit  w-[90%] mx-auto rounded outline-none `}>
+      className={`flex gap-[1px] p-2 flex-wrap h-fit mx-auto rounded w-fit outline-none select-none`}>
       {Array.from({ length: total }).map((_, index) => {
         return (
           <div key={index} className={`size-2 ${color} rounded-full`}></div>
